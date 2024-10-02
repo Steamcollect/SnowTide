ed4a7e246ca818c6351d7efed1b3c4972613dfa0
Verdraks <132646355+Verdraks@users.noreply.github.com>
2024-10-02 14:32:39 +0200

    Feat: Button Test Reset

--- Assets/Scripts/GameManagements/GameOverManager.cs
+++ Assets/Scripts/GameManagements/GameOverManager.cs
@@ -4,7 +4,8 @@ using UnityEngine.SceneManagement;
 public class GameOverManager : MonoBehaviour
 {
     [SerializeField] RSE_EventBasic rse_onDeath;
-    [SerializeField] GameObject gameOverPanel;
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private GameObject buttonPanel;
 
     private void OnEnable() => rse_onDeath.action += OnDeath;
 
@@ -17,6 +18,7 @@ public class GameOverManager : MonoBehaviour
 
     public void OnDeath()
     {
+        buttonPanel.SetActive(false);
         gameOverPanel.SetActive(true);
     }
 }
