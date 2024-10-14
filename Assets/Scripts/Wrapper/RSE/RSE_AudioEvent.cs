fc5ca22fd0cffdad102e93c84781b63af4ad8353
Orlando MARTINEZ <o_martinez1@stu-bellecour.fr>
2024-10-14 12:09:55 +0200

    Feat: work on audio syst

---
+++ Assets/Scripts/Wrapper/RSE/RSE_AudioEvent.cs
@@ -0,0 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_AudioEvent", menuName = "RSE/AudioEvent")]
+public class RSE_AudioEvent : ScriptablesObject.WrapperAction<AudioClip,float>{}
