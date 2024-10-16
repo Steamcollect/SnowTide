e357cd4934a89d5b4b282dbd713f5d6135d97ef2
Verdraks <132646355+Verdraks@users.noreply.github.com>
2024-10-16 15:50:32 +0200

    Fix: Death event fix

--- Assets/Scripts/GameManagements/GameManager.cs
+++ Assets/Scripts/GameManagements/GameManager.cs
@@ -7,10 +7,21 @@ using UnityEngine.Serialization;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private RSE_IntEvent OnPlayerTakeDamage;
     [SerializeField] private RSE_Event OnCommandSave;
     [SerializeField] private UnityEvent BackgroundMusicEvent;
     
     private void Start() => BackgroundMusicEvent.Invoke();
+    
+    private void OnEnable(){OnPlayerTakeDamage.action+= PlayerTakeDamage;}
+    private void OnDisable(){OnPlayerTakeDamage.action -= PlayerTakeDamage;}
+
+    private void PlayerTakeDamage(int currentHealth)
+    {
+        
+    }
+    
     
     private void OnApplicationQuit()
     {
