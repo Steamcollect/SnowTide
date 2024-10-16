6ab8269777e3e51cc332c9d3e16bda9a4cc0ceca
Verdraks <132646355+Verdraks@users.noreply.github.com>
2024-10-16 11:34:58 +0200

    Feat: Game Manager + save data on quit application

--- Assets/AssetsImports/BoxTool/Script/Save/LoadSaveData.cs
+++ Assets/AssetsImports/BoxTool/Script/Save/LoadSaveData.cs
@@ -64,5 +64,6 @@ namespace BT.Save
         {
             return contentSaved;
         }
+        
     }   
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using BT.Save;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private RSE_Event OnCommandSave;
+    
+    private void OnApplicationQuit()
+    {
+        OnCommandSave.Call();
+    }
 }
