21b3a93fda7afa53f774e7828f1cf31a634fb25b
Verdraks <132646355+Verdraks@users.noreply.github.com>
2024-10-31 17:16:21 +0100

    Fix: remove rseGet data save and move into rso data saved

--- Assets/AssetsImports/BoxTool/Script/Wrapper/RSE/RSE_GetDataSave.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-namespace BT.Save
-{
-    [CreateAssetMenu(fileName = "RSE_GetDataSave",menuName = "BoxTool/Save/GetDataSave")]
-    public class RSE_GetDataSave : ScriptablesObject.WrapperActionCallback<ContentSaved> {}
-}
-
