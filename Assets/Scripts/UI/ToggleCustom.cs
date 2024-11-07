c27ac4f68ce7c5c80b7f2788b3580b6ffbcb3bb5
Verdraks <132646355+Verdraks@users.noreply.github.com>
2024-11-07 15:59:19 +0100

    Fix: toggle + menu

--- Assets/Scripts/UI/ToggleCustom.cs
+++ Assets/Scripts/UI/ToggleCustom.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using BT.Audio;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class ToggleCustom : MonoBehaviour, IPointerClickHandler
 {
-    
     [SerializeField] private UnityEvent<float> onValueChanged; 
     [SerializeField] private Image _image;
     
+    [SerializeField] private SoundType _soundType;
     [SerializeField] private Sprite _defaultSprite;
     [SerializeField] private Sprite _selectedSprite;
+    [SerializeField] private AudioMixer audioMixer;
+
+    private void OnEnable()
+    { 
+        UpdateVolumeClick();   
+    }
+    
+    private void UpdateVolumeClick()
+    {
+        var nameParameter = _soundType switch
+        {
+            SoundType.Sound => "SoundVolume",
+            SoundType.Music => "MusicVolume",
+            _ => ""
+        };
+        if (nameParameter == "") return;
+        float value;
+        audioMixer.GetFloat(nameParameter,out value);
+        OnClick(!Mathf.Approximately(value, -80f));
+    }
 
     private void OnClick(bool isSelected)
     {
